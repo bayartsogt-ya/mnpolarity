5cb4752eaceb435e0143f36ff53c9bd7f60d6275
Bayartsogt Yadamsuren <bayartsogtyadamsuren@icloud.com>
2021-06-05 22:22:46 -0500

    new commit

---
+++ src/config.py
@@ -0,0 +1,15 @@
+import os
+from .utils import read_config
+
+# --------------- Configuration ---------------
+package_dir = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "..",
+)
+
+config = read_config(
+    os.path.join(package_dir, "configs", "main.yml")
+)
+
+config["package_dir"] = package_dir
+config["data_dir"] = os.path.join(package_dir, "data")
