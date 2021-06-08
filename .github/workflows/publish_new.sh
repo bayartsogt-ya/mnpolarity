070a1855df0181d38ef821711317ba5a7a1662bc
Bayartsogt Yadamsuren <bayartsogtyadamsuren@icloud.com>
2021-06-08 01:30:48 -0500

    fix for better doc

--- .github/workflows/publish_new.sh
+++ .github/workflows/publish_new.sh
@@ -16,5 +16,9 @@ cd output
 zip -r $LATEST_OUTPUT_DIR.zip $LATEST_OUTPUT_DIR
 cd ..
 
+# prepare log file as code-like doc
+sed -i -e '1s/^/```\n/' train_log.txt
+echo '```' >> train_log.txt
+
 # release
 gh release create 0.0.$VERSION output/$LATEST_OUTPUT_DIR.zip -t 0.0.$VERSION -F train_log.txt
