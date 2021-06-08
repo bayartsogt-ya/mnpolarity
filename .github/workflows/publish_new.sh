a6b1b678e18cf05be83ef830edfef28a95496cc1
Bayartsogt Yadamsuren <bayartsogtyadamsuren@icloud.com>
2021-06-08 00:48:16 -0500

    getting ready for version 6

--- .github/workflows/publish_new.sh
+++ .github/workflows/publish_new.sh
@@ -1,15 +1,20 @@
-git pull
+# get all the tags
+git fetch --prune --unshallow --tags
 
+# get latest minor version
 VERSION=$(git tag | tail -1 | tr "." "\n" | tail -1)
 ((VERSION++))
 
+# get last trained model dir
 LATEST_OUTPUT_DIR=$(ls output | tail -1)
 
 echo version 0.0.$VERSION
 echo output dir $LATEST_OUTPUT_DIR
 
+# compress
 cd output
 zip -r $LATEST_OUTPUT_DIR.zip $LATEST_OUTPUT_DIR
 cd ..
 
+# release
 gh release create 0.0.$VERSION output/$LATEST_OUTPUT_DIR.zip -t 0.0.$VERSION -F train_log.txt
